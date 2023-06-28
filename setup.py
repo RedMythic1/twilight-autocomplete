d099560e0daf6ad6a0faefaef6ecd06e4f1cbd44
RedMythic <ab7552@pleasantonusd.net>
2023-06-28 14:04:13 -0700

    4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='twilight-autocomplete',
-    version='1.0.0',
+    version='1.0.1',
     description='Autocomplete Package built using NLTK for the twilight messaging client',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
